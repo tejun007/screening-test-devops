eadb6297582b49591945fbc80fa2c6c7e9fc8f98
Taejun Lee <tejunlee007@gmail.com>
2019-07-18 22:48:10 +0900

    UPDATE: set git config

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+git config --global user.email "tejunlee007@gmail.com"
+git config --global user.name "Taejun Lee"
+
 git add .
 git commit -m "merge dev to master"
 git push origin master
