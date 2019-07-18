d4322f3261c0b1e1663707a0f696ac23b8226e18
Taejun Lee <tejunlee007@gmail.com>
2019-07-18 21:29:50 +0900

    UPDATE: add tejunlee007 at the beginning of the tag on building step

--- push.sh
+++ push.sh
@@ -22,6 +22,6 @@
 #
 #systemctl restart docker
 
-docker build --tag screening-test-backend:latest ./backend
+docker build --tag tejunlee007/screening-test-backend:latest ./backend
 docker push tejunlee007/screening-test-backend:latest
 
