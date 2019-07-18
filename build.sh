4dfb2ebb295511842e1e202280b4c73ebe098ed4
Taejun Lee <tejunlee007@gmail.com>
2019-07-18 22:44:01 +0900

    UPDATE: build only on build stage

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker rmi tejunlee007/screening-test-backend
+docker rmi tejunlee007/screening-test-backend:latest
 docker build --tag tejunlee007/screening-test-backend:latest ./backend
