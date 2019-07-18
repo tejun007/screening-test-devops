b656f19fa2483d036f804d2072e5c4fc63d84af2
Taejun Lee <tejunlee007@gmail.com>
2019-07-18 18:46:59 +0900

    UPDATE: edit push.sh

--- push.sh
+++ push.sh
@@ -12,10 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get update
 
